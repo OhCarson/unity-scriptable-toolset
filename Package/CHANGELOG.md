a183437114a1a4cfb8cd2e1d3eb156bfdce748f1
Oliver Carson <ocarson@gmail.com>
2024-04-05 16:55:47 +0100

    Float to timespan added for timers

--- Package/CHANGELOG.md
+++ Package/CHANGELOG.md
@@ -7,17 +7,25 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [0.0.1] - 2024-02-27
 ### Added
 - Inital release
+
 ## [0.0.11] - 2024-02-28
 ### Added
 - Fixed builds by adding a seporate editor assembly
+
 ## [0.0.12] - 2024-03-20
 ### Added
 - A Timer, Variable change response and advanced text replacing scripts added
+
 ## [0.0.13] - 2024-03-28
 ### Added
 - Comparision event raisers added for general, string, and float variables
-## [0.0.14] - 2024-03-28
+
+## [0.0.14] - 2024-04-01
 ### Added
 - BoolVariable
 ### Fixed
 - ToString and comparison of reference variables
+
+## [0.0.15] - 2024-04-05
+### Added
+- Float to timespan text for dealing with countdowns and timers
---
+++ UnityProject/ScriptableToolset/Packages/ScriptableToolset/Runtime/Variables/UI/TextFloatToTimeSpan.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+namespace Scriptable.Variables
+{
+    public class TextFloatToTimeSpan : MonoBehaviour
+    {
+        public TMP_Text Label;
+
+        public FloatReference Variable;
+        public enum DisplayRange { Seconds, Minutes, Hours, Days };
+
+        public DisplayRange displayRange;
+        public bool DisplayMilliseconds = false;
+
+        //replace {t} with text
+        public string Text = "{t}";
+
+        public bool AlwaysUpdate = true;
+
+        private void OnEnable()
+        {
+            Label.text = FormatFloat();
+        }
+
+        private void Update()
+        {
+            if (AlwaysUpdate)
+            {
+                Label.text = FormatFloat();
+            }
+        }
+
+        public string FormatFloat()
+        {
+            var from = new DateTime();
+            var to = from.AddSeconds(Variable.Value);
+            var timeSpan = to - from;
+
+            var timeText = "";
+
+            switch(displayRange)
+            {
+                case DisplayRange.Seconds:
+                    timeText = ((int)timeSpan.TotalSeconds).ToString("D2");
+                    break;
+                case DisplayRange.Minutes:
+                    timeText = ((int)timeSpan.TotalMinutes).ToString("D2") + ":" +
+                        ((int)timeSpan.Seconds).ToString("D2");
+                    break;
+                case DisplayRange.Hours:
+                    timeText = ((int)timeSpan.TotalHours).ToString() + ":" +
+                        ((int)timeSpan.Minutes).ToString("D2") + ":" +
+                        ((int)timeSpan.Seconds).ToString("D2");
+                    break;
+                case DisplayRange.Days:
+                    timeText = ((int)timeSpan.TotalDays).ToString("D2") + ":" +
+                        ((int)timeSpan.Hours).ToString("D2") + ":" +
+                        ((int)timeSpan.Minutes).ToString("D2") + ":" +
+                        ((int)timeSpan.Seconds).ToString("D2");
+                    break;
+            }
+
+            if(DisplayMilliseconds)
+                timeText = timeText + "." + ((int)timeSpan.Milliseconds).ToString("D2").Substring(0,2);
+
+            return Text.Replace("{t}", timeText);
+        }
+    }
+}
